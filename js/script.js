4200a227d965ffbef603739d4341b6a8b870d429
lovely <ldde07@gmail.com>
2019-05-01 16:51:48 -0700

    Update on script using Ajaxx

--- js/script.js
+++ js/script.js
@@ -1,6 +1,18 @@
-function helloWorld(){
-console.log('hello world');
+function loadArticles(category){
+    console.log(category);
+    $.ajax({
+        method: "get",
+        url: "https://api.nytimes.com/svc/topstories/v2/"+category+".json?api-key=s5AF7dJT7Aw8VOkkVQ0g0LSWD8xiwxp2"
+      }).done(function(data) {
+        console.log(data);
+      });
 }
-helloWorld();
 
-console.log('test');
+  $('#choose-section-menu').on('change', function() {
+    const selected = $(this).val();
+    if (selected !== '') {
+      console.log('The value you picked is: ' + selected);
+      loadArticles(selected);
+      $('.articles').append();
+    }
+  });
