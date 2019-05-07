6c3bb0cc6a99b323416713cdb2c3775056a701c7
lovely <ldde07@gmail.com>
2019-05-06 17:08:36 -0700

    Update shrink and grow

--- js/script.js
+++ js/script.js
@@ -10,18 +10,32 @@
      * Change event for select list and call .ajax
      */
     $('#choose-menu').on('change', function() {
+      $('.site-header').addClass('shrink');
+      $('.main-article').addClass('grow');
+      
       const selected = $(this).val();
       if (selected !== '') {
+        
         console.log('The value you picked is: ' + selected);
+      //on change 
+ 
+
+
 
         $.ajax({
           method: "get",
           url: "https://api.nytimes.com/svc/topstories/v2/"+selected+".json?api-key=s5AF7dJT7Aw8VOkkVQ0g0LSWD8xiwxp2"
         }).done(function(data) {
           console.log(data); 
-          // $('grid-article'),html('');
+          $('ul').html('');
+         
+          
+          // filter data.results
+          //put slice the filtered data.results, this will be a new array
           $.each(data.results, function( index, article ){
             console.log(article);
+         
+       
 
             const articleWrap = `
           <li class="list-article">
@@ -34,6 +48,7 @@
           </a>
           </li>
   `;
+ 
   $('ul').append(articleWrap);
 });
 
@@ -42,19 +57,12 @@
 
 
 
-            
-          //   $('.article').append(`
-          //     <ul>${article.title} + $</ul>
-          //   `);
-          // }); // end of .each
-
-          // template string `<p>${variable}</p>`
-          // easier than concatination which is "<p>" + variable + "</p>"
-    // console.log('The value you picked is: ' + selected)
+      
         });
 
         // loadArticles(selected);
         // $('.articles').append();
+   
       }// end if selected !==''
     });// end of change event
 
