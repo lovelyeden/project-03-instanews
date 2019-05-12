ddf4f567a36d47d1402ba0317d9f4fc3d310b67e
lovely <ldde07@gmail.com>
2019-05-12 15:06:45 -0700

    updated and clean the codee

--- js/script.js
+++ js/script.js
@@ -1,72 +1,59 @@
+/* Change event for select list and call .ajax */
+$("#choose-menu").on("change", function() {
+  $(".site-header").addClass("shrink");
+  $(".main-article").addClass("grow");
 
+  const selected = $(this).val();
+  if (selected !== "") {
+    console.log("The value you picked is: " + selected);
 
-      // chaining
-      // $.ajax().done().fail().always();
-      
-     
-  
-  
-    /* Change event for select list and call .ajax */
-    $('#choose-menu').on('change', function() {
-      $('.site-header').addClass('shrink');
-      $('.main-article').addClass('grow');
-      
-      const selected = $(this).val();
-      if (selected !== '') {
-        
-        console.log('The value you picked is: ' + selected);
-      //on change 
- 
-        $('.loader').show();
-  
+    $(".loader").show(); // A call for loader to show
 
-        $.ajax({
-          method: "get",
-          url: "https://api.nytimes.com/svc/topstories/v2/"+selected+".json?api-key=s5AF7dJT7Aw8VOkkVQ0g0LSWD8xiwxp2"
-        }).done(function(data) {
-          const articleFiltered = data.results.filter(function(stories){
-          return stories.multimedia[4] !==  undefined;
-          }).slice(0,12);
-          $('ul').html('');
-          $.each(articleFiltered, function( index, article ){
+    $.ajax({
+      method: "get",
+      url:
+        "https://api.nytimes.com/svc/topstories/v2/" +
+        selected +
+        ".json?api-key=s5AF7dJT7Aw8VOkkVQ0g0LSWD8xiwxp2"
+    })
+      .done(function(data) {
+        const articleFiltered = data.results
+          .filter(function(stories) {
+            return stories.multimedia[4] !== undefined;
+          })
+          .slice(0, 12);
+        $("ul").html("");
+        $.each(articleFiltered, function(index, article) {
           console.log(article);
-         
 
+          //
           const articleWrap = `
-          <li class="list-article" style="background-image:url(${article.multimedia[4].url}); background-size: cover;">
-          <a href=${article.url} "target="_blank">
+          <li class="list-article">
+          <a href=${article.url} 
+          "target="_blank" style="background-image:url(${
+            article.multimedia[4].url
+          });
+          background-size:cover;" >
           <div class="stories-container">
           <p class="headlines">${article.abstract}</p>
           </div>
-          </div>
           </a>
           </li>
-  `;
-  $('ul').append(articleWrap);
-});
-})
-.fail(function() {
-$('.main-article').append('<p class="error">Sorry there was an error in loading the page.</p>');
-})
-.always(function(){
-  $('.loader').hide();
-
-});
-            
-
-
-
-
-      
-
-        
-
-        // loadArticles(selected);
-        // $('.articles').append();
-   
-      }// end if selected !==''
-    });// end of change event
+          `;
+          $("ul").append(articleWrap);
+        });
+      })
+      .fail(function() {
+        $(".main-article").html("");
+        $(".main-article").append(
+          '<p class="error">Sorry there was an error in loading the page.</p>'
+        );
+      })
+      .always(function() {
+        $(".loader").hide();
+      });
 
 
+  } 
+}); 
 
-  // }
